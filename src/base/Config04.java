b0035f7b8cd9ec7504af6ce19804917228e94077
MD Alam <alammd1989@gmail.com>
2019-10-20 04:02:28 -0400

    newPush

--- src/base/Config04.java
+++ src/base/Config04.java
@@ -8,7 +8,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Parameters;
-import org.testng.annotations.Test;
+
 
 import util.WebDriverAPI02;
 
